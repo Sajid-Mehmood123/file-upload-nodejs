4b04bac51dd5fa0eb5fd58a7d8b7b7be43ad1c36
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-13 23:12:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connection");
+
+app.get("/", (req, res) => {
+  res.send("<h1>File upload starter</h1>");
+});
+
+// middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
